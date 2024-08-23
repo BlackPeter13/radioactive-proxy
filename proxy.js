ba6027dbae297b6d5d5d0eff2bf0c2626cfe3bfb
BlackPeter13 <peterswebdesign2010@gmail.com>
2024-08-23 16:12:38 +0200

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,36 +1,29 @@
 const express = require('express');
 const axios = require('axios');
+const fs = require('fs');
+const { getPool, logStats } = require('./poolManager');
+const { logRequest } = require('./stats');
 
 const app = express();
 app.use(express.json());
 
-const pool1Url = 'http://pool1.example.com';
-const pool2Url = 'http://pool2.example.com';
-
 app.post('/mine', async (req, res) => {
     const minerData = req.body;
+    const pool = getPool();  // Load balancing to decide which pool to use
 
     try {
-        // Send the mining work to Pool 1
-        const pool1Response = await axios.post(`${pool1Url}/work`, minerData);
-        
-        // Send the mining work to Pool 2
-        const pool2Response = await axios.post(`${pool2Url}/work`, minerData);
-        
-        // Merge the work responses from both pools
-        const mergedWork = {
-            pool1: pool1Response.data,
-            pool2: pool2Response.data,
-        };
+        const response = await axios.post(`${pool.url}/work`, minerData);
 
-        // Send the merged work back to the ASIC miner
-        res.json(mergedWork);
+        logRequest(pool.name, minerData, response.data);  // Log request and response
+        res.json(response.data);
 
     } catch (error) {
-        console.error('Error processing mining work:', error);
+        console.error(`Error sending work to ${pool.name}:`, error);
         res.status(500).send('Error processing mining work');
     }
 });
+
+app.use(express.static('../frontend'));  // Serve the frontend
 
 app.listen(3000, () => {
     console.log('Merged Mining Proxy running on port 3000');
